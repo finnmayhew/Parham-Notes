41fa52b09bc2f5674984725cd024b09c46296b4c
Mayhew, Finn <mayhewfi@msu.edu>
2023-11-18 14:36:21 -0500

    Quartz sync: Nov 18, 2023, 2:36 PM

--- content/index.md
+++ content/index.md
@@ -1,4 +1,3 @@
+To see all the files,  click the button in the top right of the graph view below.
 ## Sessions
 - [[Session 1]]
-
-Test
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,30 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        drag: true, // whether to allow panning the view around
+        zoom: true, // whether to allow zooming in and out
+        depth: 1, // how many hops of notes to display
+        scale: 1.1, // default view scale
+        repelForce: 0.5, // how much nodes should repel each other
+        centerForce: 0.3, // how much force to use when trying to center the nodes
+        linkDistance: 30, // how long should the links be by default?
+        fontSize: 0.3, // what size should the node labels be?
+        opacityScale: 0.5, // how quickly do we fade out the labels when zooming out?
+      },
+      globalGraph: {
+        drag: true,
+        zoom: true,
+        depth: -1,
+        scale: 0.9,
+        repelForce: 0.5,
+        centerForce: 0.3,
+        linkDistance: 30,
+        fontSize: 0.3,
+        opacityScale: 0.5,
+      },
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
